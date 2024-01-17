0e93d65908e1aebed2885a28833b92c3d452bf05
Ryan Lerch <rlerch@redhat.com>
2024-01-17 11:58:43 +1000

    add tox and add linting checks
    Signed-off-by: Ryan Lerch <rlerch@redhat.com>

--- pagure_notifications/__init__.py
+++ pagure_notifications/__init__.py
@@ -1,13 +1,13 @@
-import hmac
 import hashlib
+import hmac
 
-from maubot import Plugin
-from aiohttp.web import Response
-from mautrix.util.config import BaseProxyConfig, ConfigUpdateHelper
 import jinja2
-import json as jsonlib
+from aiohttp.web import Response
+from maubot import Plugin
+from mautrix.util.config import BaseProxyConfig, ConfigUpdateHelper
 
 ACCEPTED_TOPICS = ["issue.new", "issue.comment.added", "git.receive"]
+
 
 class Config(BaseProxyConfig):
     def do_update(self, helper: ConfigUpdateHelper):
@@ -15,8 +15,7 @@ class Config(BaseProxyConfig):
         helper.copy("projects")
 
 
-class PagureNotifications(Plugin):    
-
+class PagureNotifications(Plugin):
     @classmethod
     def get_config_class(cls):
         return Config
@@ -25,51 +24,55 @@ class PagureNotifications(Plugin):
         self.config.load_and_update()
         self.webapp.add_route("POST", "/notify", self.handle_request)
         self.log.info(f"Webhook URL is: {self.webapp_url}notify")
-        self.log.error(self.config["projects"])      
+        print(self.webapp_url)
+        self.log.error(self.config["projects"])
 
     async def handle_request(self, request):
-
         json = await request.json()
-        self.log.error(jsonlib.dumps(json, indent=2))
         message_topic = json.get("topic")
 
         # first check if the topic of the message is one of the ones that the plugin handles
         if message_topic not in ACCEPTED_TOPICS:
             return Response()
-        
+
         try:
             if message_topic == "git.receive":
                 project_name = json["msg"]["project_fullname"]
             else:
                 project_name = json["msg"]["project"]["fullname"]
         except KeyError as e:
-            self.log.error(f'The response from pagure was not as expected: {e}.')
+            self.log.error(f"The response from pagure was not as expected: {e}.")
             return Response()
 
         if project_name not in self.config["projects"]:
-            self.log.error(f'project {project_name} is sending me a webhook, but it is not defined in my config!')
+            self.log.error(
+                f"project {project_name} is sending me a webhook, "
+                f"but it is not defined in my config!"
+            )
             return Response()
-        
+
         try:
             key = self.config["projects"][project_name]["key"]
             topics = self.config["projects"][project_name]["topics"]
             rooms = self.config["projects"][project_name]["topics"][message_topic]
         except KeyError as e:
-            self.log.error(f'Project configuation for the plugin is invalid: {e}.')
+            self.log.error(f"Project configuation for the plugin is invalid: {e}.")
             return Response()
 
         content = await request.read()
         hashhex = hmac.new(key.encode(), msg=content, digestmod=hashlib.sha1).hexdigest()
 
-        if not hmac.compare_digest(hashhex, request.headers.get('X-Pagure-Signature')):
-            self.log.error(f'message from project {project_name} did not validate correctly. ignoring')
+        if not hmac.compare_digest(hashhex, request.headers.get("X-Pagure-Signature")):
+            self.log.error(
+                f"message from project {project_name} did not validate correctly. ignoring"
+            )
             return Response()
 
         if message_topic not in topics:
             return Response()
 
         template = self.loader.sync_read_file(f"pagure_notifications/{message_topic}.j2")
-        message = jinja2.Template(template.decode()).render({'json': json})
+        message = jinja2.Template(template.decode()).render({"json": json})
 
         for room in rooms:
             if room[0] == "#":
