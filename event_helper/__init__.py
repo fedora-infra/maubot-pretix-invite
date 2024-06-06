dc4c8d95168f2c4454453ed70073ed62f75e7cfc
Adrian Edwards <moralcode@fedoraproject.org>
2024-06-06 13:53:05 -0400

    implement permission check by username

--- event_helper/__init__.py
+++ event_helper/__init__.py
@@ -130,14 +130,17 @@ class EventManagement(Plugin):
     @command.new(name="batchinvite", help="invite from a pretix URL")
     @command.argument("pretix_url", pass_raw=True, required=True)
     async def batchinvite(self, evt: MessageEvent, pretix_url: str) -> None:
-        # Ensure room exists
-        # room_id = await self.matrix_utils.ensure_room_with_alias(alias)
-        room_id = evt.room_id
-        # Ensure users are invited
-        all_users = {}
-        # all_users.update({username: UserInfo()})
-        all_users[username] = UserInfo()
-        await self.matrix_utils.ensure_room_invitees(room_id, all_users)
+        # permission check
+        # sender = evt.sender
+        if evt.sender in self.config["allowlist"]:
+            # Ensure room exists
+            # room_id = await self.matrix_utils.ensure_room_with_alias(alias)
+            room_id = evt.room_id
+            # Ensure users are invited
+            all_users = {}
+            # all_users.update({username: UserInfo()})
+            all_users[username] = UserInfo()
+            await self.matrix_utils.ensure_room_invitees(room_id, all_users)
 
-        # Ensure users have correct power levels
-        # await self.matrix_utils.ensure_room_power_levels(room_id, all_users)
+            # Ensure users have correct power levels
+            # await self.matrix_utils.ensure_room_power_levels(room_id, all_users)
