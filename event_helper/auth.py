c9315fa6ab80a5649095f17fed6d78ee6d110da0
Adrian Edwards <moralcode@fedoraproject.org>
2024-06-07 12:40:53 -0400

    update token parsing and loading based on actual tokens received from pretix

--- event_helper/auth.py
+++ event_helper/auth.py
@@ -10,21 +10,25 @@ class Token:
     access_token: str
     refresh_token: str
     token_type: str
+    scope: list[str]
     expires_at: datetime
 
 
     @classmethod
     def from_str(cls, string):
-        return cls.from_json(json.parse(string))
+        return cls.from_json(json.loads(string))
 
     @classmethod
     def from_json(cls, json):
-        now = datetime.now()
-        exires_in = timedelta(seconds=json["expires_in"])
-        expiry = now + exires_in
+        if json.get("expires_at") is not None:
+            expiry = datetime.fromtimestamp(json.get("expires_at"), tz=timezone.utc)
+        else:
+            now = datetime.now()
+            exires_in = timedelta(seconds=json["expires_in"])
+            expiry = now + exires_in
         return cls(json["access_token"], json["refresh_token"], json["token_type"], expiry)
 
-    def to_json(self):
+    def to_json(self) -> str:
         expires_at = self.expires_at
         now = datetime.now()
         exires_in = expires_at - now
@@ -32,4 +36,5 @@ class Token:
         data = asdict(self)
         del data["expires_at"]
         data["expires_in"] = expires_in.seconds
+        data["expires_at"] = expires_at.timestamp()
         return json.dumps(data, default=str)
