f62f688e509cd76de26b735215bf4a9ed18f27f4
Adrian Edwards <moralcode@fedoraproject.org>
2024-06-14 14:17:31 -0400

    prints

--- event_helper/__init__.py
+++ event_helper/__init__.py
@@ -241,7 +241,9 @@ class EventManagement(Plugin):
     def _events_for_room(self, room):
         events = []
         for organizer in self.room_mapping:
+            self.log.debug(organizer)
             for event in self.room_mapping[organizer]:
+                self.log.debug(event)
                 if room in event:
                     events.append(f"{organizer}/{event}")
         return events
