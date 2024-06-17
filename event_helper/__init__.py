095ff5221ec115c2d8659810aefdc7c669b832f3
Adrian Edwards <moralcode@fedoraproject.org>
2024-06-17 14:31:34 -0400

    ah yep correctly mark invited attendees as processed

--- event_helper/__init__.py
+++ event_helper/__init__.py
@@ -140,7 +140,7 @@ class EventManagement(Plugin):
 
             # this assumes we are only really processing one new attendee at a time
             if len(failed_invites) == 0:
-                self.pretix.mark_as_processed(result)
+                self.pretix.mark_as_processed(attendees)
             else:
                 self.log.error(f"unable to invite member {matrix_id}")
 
