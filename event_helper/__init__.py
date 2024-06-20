6d86861081d808628b37f34f8d598bbfbb6c7c9a
Adrian Edwards <moralcode@fedoraproject.org>
2024-06-20 15:43:52 -0400

    move eventrooms to a separate file

--- event_helper/__init__.py
+++ event_helper/__init__.py
@@ -2,7 +2,6 @@ import hashlib
 from itertools import chain
 import hmac
 from typing import List
-from dataclasses import dataclass, field
 
 import jinja2
 from aiohttp.web import Response
@@ -15,6 +14,7 @@ from mautrix.util.config import BaseProxyConfig, ConfigUpdateHelper
 
 from .matrix_utils import MatrixUtils, UserInfo, validate_matrix_id
 from .pretix import Pretix, AttendeeMatrixInformation
+from .eventrooms import EventRooms
 # ACCEPTED_TOPICS = ["issue.new", "git.receive", "pull-request.new"]
 
 NL = "      \n"
@@ -26,60 +26,6 @@ class Config(BaseProxyConfig):
         helper.copy("pretix_client_secret")
         helper.copy("pretix_redirect_url")
         helper.copy("allowlist")
-
-@dataclass
-class EventRooms:
-    _mapping: dict = field(default_factory=lambda: {})
-
-    def rooms_by_event(self, organizer:str, event:str):
-        if self._mapping.get(organizer) is None:
-            return set()
-        
-        if self._mapping[organizer].get(event) is None:
-            return set()
-        
-        return self._mapping[organizer].get(event)
-    
-    def add(self, organizer:str, event:str, room_id:str):
-        if self._mapping.get(organizer) is None:
-            self._mapping[organizer] = {} 
-        
-        if self._mapping[organizer].get(event) is None:
-            self._mapping[organizer][event] = set()
-        
-        self._mapping[organizer][event].add(room_id)
-
-    def remove(self, organizer:str, event:str, room_id:str):
-        if room_id in self.rooms_by_event(organizer,event):
-            self._mapping[organizer][event].remove(room_id)
-
-
-    def room_is_mapped(self, room:str):
-        return len(self.events_for_room(room)) > 0
-
-    def events_for_room(self, room:str):
-        """return a list of events that a room is mapped to in "organizer/event" format
-
-        Args:
-            room (str): the id of the room to return events for
-
-        Returns:
-            List[str]: the list of events the room is part of
-        """
-        events = []
-        for organizer in self._mapping:
-            for event in self._mapping[organizer]:
-                if room in self._mapping[organizer][event]:
-                    events.append(f"{organizer}/{event}")
-        return events
-    
-    def purge_room(self, room):
-        """remove a room from all events it is mapped to
-        """
-        for organizer in self._mapping:
-            for event in self._mapping[organizer]:
-                if room in event:
-                    self._mapping[organizer][event].remove(room)
 
 
 class EventManagement(Plugin):
