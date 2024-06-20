781394b3171749a34ae15b5a04a9c9ea2673ca7b
Adrian Edwards <moralcode@fedoraproject.org>
2024-06-20 16:13:07 -0400

    pull method signatures into an interface style class

--- event_helper/eventrooms.py
+++ event_helper/eventrooms.py
@@ -1,5 +1,36 @@
 
 class EventRooms:
+
+    def rooms_by_event(self, organizer:str, event:str):
+        raise NotImplementedError()
+
+    def add(self, organizer:str, event:str, room_id:str):
+        raise NotImplementedError()
+
+    def remove(self, organizer:str, event:str, room_id:str):
+        raise NotImplementedError()
+
+    def room_is_mapped(self, room:str):
+        raise NotImplementedError()
+
+    def events_for_room(self, room:str):
+        """return a list of events that a room is mapped to in "organizer/event" format
+
+        Args:
+            room (str): the id of the room to return events for
+
+        Returns:
+            List[str]: the list of events the room is part of in "organizer/event" string format
+        """
+        raise NotImplementedError()
+
+    def purge_room(self, room):
+        """remove a room from all events it is mapped to
+        """
+        raise NotImplementedError()
+
+
+class EventRoomsMemory(EventRooms):
     _mapping: dict = {}
 
     def rooms_by_event(self, organizer:str, event:str):
@@ -29,14 +60,6 @@ class EventRooms:
         return len(self.events_for_room(room)) > 0
 
     def events_for_room(self, room:str):
-        """return a list of events that a room is mapped to in "organizer/event" format
-
-        Args:
-            room (str): the id of the room to return events for
-
-        Returns:
-            List[str]: the list of events the room is part of
-        """
         events = []
         for organizer in self._mapping:
             for event in self._mapping[organizer]:
@@ -45,8 +68,6 @@ class EventRooms:
         return events
     
     def purge_room(self, room):
-        """remove a room from all events it is mapped to
-        """
         for organizer in self._mapping:
             for event in self._mapping[organizer]:
                 if room in event:
