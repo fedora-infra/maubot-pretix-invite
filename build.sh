9858474ca0fb5f4bdaa17617bbfc5f2c1484c185
Adrian Edwards <moralcode@fedoraproject.org>
2024-07-05 09:42:54 -0400

    adjust script to hopefully run the maubot build step

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-image="docker.io/antora/antora"
-cmd="--html-url-extension-style=indexify site.yml"
+# Run a maubot build to package this plugin into a .mbc file for upload to a maubot server
+
+image="dock.mau.dev/maubot/maubot"
+cmd="mbc build"
 
 if uname | grep -iwq darwin; then
     # Running on macOS.
@@ -10,23 +12,25 @@ if uname | grep -iwq darwin; then
     echo ""
     echo "This build script is using Docker container runtime to run the build in an isolated environment."
     echo ""
-    docker run --rm -it -v "$(pwd):/antora" "${image}" ${cmd}
+    docker run --rm -it -v "$(pwd):/maubot-events" "${image}" ${cmd}
 
 elif uname | grep -iq linux; then
     # Running on Linux.
-    # there isn't an antora/aarch64 container, antora can be installed locally
+    # there isn't a maubot aarch64 container (TODO: maybe fall back to using a python environment with maubot to run the build in this case)
     # Check whether podman is available, else fall back to docker
     # which requires root.
 
-    if [ -f /usr/local/bin/antora ]; then
-        /usr/local/bin/antora "${cmd}"
-    elif uname -m | grep -iwq aarch64; then
-        echo "no antora/aarch64 container try just \`npm install -g @antora/cli @antora/site-generator-default\`"
+    if uname -m | grep -iwq aarch64; then
+        echo "no maubot aarch64 container. Falling back to python (experimental/untested)"
+        pip install asyncpg maubot maubot-fedora-messages mautrix pytz
+        pip install -r requirements.txt
+        python3 -m maubot.cli mbc build
+
     elif [ -f /usr/bin/podman ]; then
         echo ""
         echo "This build script is using Podman to run the build in an isolated environment."
         echo ""
-        podman run --rm -it -v "$(pwd):/antora:z" "${image}" ${cmd}
+        podman run --rm -it -v "$(pwd):/maubot-events:z" "${image}" ${cmd}
 
     elif [ -f /usr/bin/docker ]; then
         echo ""
@@ -34,14 +38,14 @@ elif uname | grep -iq linux; then
         echo ""
 
         if groups | grep -wq "docker"; then
-            docker run --rm -it -v "$(pwd):/antora:z" "${image}" ${cmd}
+            docker run --rm -it -v "$(pwd):/maubot-events:z" "${image}" ${cmd}
         else
             echo "You might be asked for your password."
             echo "You can avoid this by adding your user to the 'docker' group,"
             echo "but be aware of the security implications."
             echo "See https://docs.docker.com/install/linux/linux-postinstall/"
             echo ""
-            sudo docker run --rm -it -v "$(pwd):/antora:z" "${image}" ${cmd}
+            sudo docker run --rm -it -v "$(pwd):/maubot-events:z" "${image}" ${cmd}
         fi
     else
         echo ""
